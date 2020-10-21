2921e2312009bfb085108ebe359f2d2662a1aaaf
gokhan <gokhan@gulenayva.com>
2020-10-21 20:16:09 +0300

    home page movie list data

--- src/components/home/movieListComponent.js
+++ src/components/home/movieListComponent.js
@@ -5,6 +5,7 @@ import {connect} from "react-redux";
 class MovieListComponent extends Component {
 
     render() {
+        console.log("this.props.searchData",this.props.searchData)
         return (
             <div className="movie-list-block">
                 <div className="head-item">
@@ -12,11 +13,14 @@ class MovieListComponent extends Component {
                         <span>Poster & Title</span>
                     </div>
                     <div className="right-item">
+                        <div className="year-item">
+                            <span>Year</span>
+                        </div>
                         <div className="id-item">
                             <span>id</span>
                         </div>
                         <div className="rating-item">
-                            <span>IMDb Rating</span>
+                            <span>Type</span>
                         </div>
                     </div>
                 </div>
--- src/components/home/movieListItemComponent.js
+++ src/components/home/movieListItemComponent.js
@@ -2,27 +2,28 @@ import React, {Component} from 'react';
 
 class MovieListItemComponent extends Component {
     render() {
+        const {Poster,Title,Year,imdbID,Type} = this.props;
         return (
             <div className="movie-list-item">
                 <div className="left-block">
                     <div className="img-item">
-                        <img src="" alt=""/>
+                        <img src={Poster} alt={Poster}/>
                     </div>
                     <div className="text-item">
                         <div className="name-item">
-                            <span>Movie name</span>
-                        </div>
-                        <div className="year-item">
-                            <span>year</span>
+                            <span>{Title}</span>
                         </div>
                     </div>
                 </div>
                 <div className="right-block">
+                    <div className="year-item">
+                        <span>{Year}</span>
+                    </div>
                     <div className="id-item">
-                        <span>id</span>
+                        <span>{imdbID}</span>
                     </div>
                     <div className="rating-item">
-                        <span>9.4</span>
+                        <span>{Type}</span>
                     </div>
                 </div>
             </div>
